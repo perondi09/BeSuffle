a0ee715720c5abf1caa64b4a0cb5d476ba10723f
perondi <guilhermeperondi09@gmail.com>
2026-03-17 23:38:48 -0300

    Feat: Making the code more secure by hiding sensitive keys.

--- src/main/java/perondi/BeShuffle/BeShuffleApplication.java
+++ src/main/java/perondi/BeShuffle/BeShuffleApplication.java
@@ -1,5 +1,6 @@
 package perondi.BeShuffle;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -7,6 +8,15 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 public class BeShuffleApplication {
+
+	static {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> {
+			if (System.getProperty(entry.getKey()) == null) {
+				System.setProperty(entry.getKey(), entry.getValue());
+			}
+		});
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeShuffleApplication.class, args);
